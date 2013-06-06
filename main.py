d6d70e4c41389c53fe9b8b77feac02e54c1b305a
Jesse Menn <jesse.menn@gmail.com>
2013-06-06 19:16:10 -0400

    Deal w/ hyphenation and 'd/'n

--- main.py
+++ main.py
@@ -42,26 +42,75 @@ def main(filename):
 	poem = [] #the poem, by line, will go here
 	poem = openFile(poem, filename)
 	poem = makeWords(poem)
-	procLine(poem)
 
-## output
-	i = 0
-	for p in poem:
-		print poem[i]
-		for word in poem[i]:
-			print word['word'].lower(), word['low'], word['high'], word['repl'], word['inDict']
-		i = i+1
+	for line in poem: 
+		procLine(line)
 
-def procLine(poem):
+# ## output
+# 	i = 0
+# 	for p in poem:
+# 		print poem[i]
+# 		for word in poem[i]:
+# 			print word['word'], word['low'], word['high'], word['repl'], word['inDict']
+# 		i = i+1
+
+def procLine(line):
 	'''
-		Recieves poem (the full poem)
-		Checks each word in the poem
+		Recieves line of poem
+		Checks each word in the line
 	'''
-	for line in poem:
-		for w in line:
-			w['inDict'] = checkDict(w)
-			if w['inDict']:
-				getSyl(w)
+	for w in line:
+		w['inDict'] = checkDict(w)
+		if (w['inDict'] == True):
+			getSyl(w)
+		elif (w['inDict'] == False):
+			temp = w['word'][:-1]
+	print line
+def getSyl(word):
+	'''
+		Takes dictionary "word." Finds min/max syl count.
+		Stores results in word['low'] and word['high'], respectively.
+		If in CMU, use that. Otherwise, use dumbGuess.
+	'''
+	lowercase = word['word'].lower()
+	if (word['inDict']):
+		word['low'] = min([len([y for y in x if isdigit(y[-1])]) for x in CMU[lowercase]])
+		word['high'] = max([len([y for y in x if isdigit(y[-1])]) for x in CMU[lowercase]])
+
+def replaceHyphen(wordA, wordB):
+	'''
+		Recieves two 'word' as dict, wordB is blank.
+		Called from makeWords.
+		Replaces hyphen with a space. Returns two values, the words pre/post-hyphen
+		Note, this is really clumsy... replace hyphen in A with a space. Set temp
+			to the split word. Split it at the space (thus making a list?). Set 
+			wordA to 1st item of temp; wordB to 2nd item. Return both words (as dict)
+	'''
+	for punct in set('-'):
+		wordA['word'] = wordA['word'].replace(punct, ' ')
+	temp = wordA['word']
+	temp = temp.split(' ')
+	wordA['word'] = temp[0]
+	wordB['word'] = temp[1]
+	return wordA, wordB
+
+def replaceStuff(word):
+	'''
+		Takes word (as dict).
+		Modifies the dictionary as needed.
+		Replaces: 'd endings with ed; 'n with en;
+	'''
+	temp = word['word'] #store original word to check if we replaced
+	if (len(word['word']) > 1):
+		if ((word['word'][-2] == "'")):
+			if ((word['word'][-1] == "d") or (word['word'][-1] == "n")): # ends in 'd, 'n
+				word['word'] = word['word'].replace(word['word'][-2], 'e')
+		for punct in string.punctuation:
+			word['word'] = word['word'].replace(punct, "") ## strip any other punctuation
+	if (word['word'] == temp): ## did we replace anything?
+		word['repl'] = False
+	else:
+		word['repl'] = True
 
 def checkDict(word):
 	'''
@@ -82,6 +131,8 @@ def makeWords(poem):
 			word: word as string
 			low: minimum syl count
 			high: minimum syl count
+			repl: if something's been replaced (like a 'd)
+			inDict: if the word is in the dictionary
 	'''
 	tempPoem = []
 	for line in poem:
@@ -89,10 +140,19 @@ def makeWords(poem):
 		for word in line:
 			temp = dict(word='', low=0, high=0, repl=False, inDict=False)
 			temp['word'] = word
-			tempLine.append(temp)
+			if '-' in temp['word']:
+				tempX = dict(word='', low=0, high=0, repl=False, inDict=False)
+				temp, tempX = replaceHyphen(temp, tempX)
+					# see replaceHyphen function for description
+				replaceStuff(temp)
+				replaceStuff(tempX)
+				tempLine.append(temp)
+				tempLine.append(tempX)
+			else:
+				replaceStuff(temp)
+				tempLine.append(temp)
 		tempPoem.append(tempLine)
 	return tempPoem
-
 
 def openFile(poem, filename):
 	'''
@@ -108,7 +168,6 @@ def openFile(poem, filename):
 		temp = nltk.WhitespaceTokenizer().tokenize(datum)
 		poem.append(temp)
 	return poem
-
 
 def printBlank(num, message):
 	''' Takes an integer and a message to print to screen for spacing
